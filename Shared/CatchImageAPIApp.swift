10e0e82b6b526ea7424bfd3844e7698cc4f95f36
Andhar7 <70223638+Andhar7@users.noreply.github.com>
2020-12-14 19:33:42 +0100

    Initial Commit

---
+++ Shared/CatchImageAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchImageAPIApp.swift
+//  Shared
+//
+//  Created by Andrej Kling on 14.12.20.
+//
+
+import SwiftUI
+
+@main
+struct CatchImageAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
